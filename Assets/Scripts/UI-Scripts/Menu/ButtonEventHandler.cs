3071d41d167203dd78b5eb1e1c866523035015d6
Mrp1 Gamer <mrp1gamerplayz@gmail.com>
2020-09-02 18:13:29 +0530

    some work

--- Assets/Scripts/UI-Scripts/Menu/ButtonEventHandler.cs
+++ Assets/Scripts/UI-Scripts/Menu/ButtonEventHandler.cs
@@ -1,23 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum MenuType { Main, Options, About, LevelSelect}
+
 public class ButtonEventHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private event Action<MenuType> DisableOthers;
+    [SerializeField] MenuType menuGroup;
+
+    private void Awake()
     {
-        
+        this.DisableOthers += MenuTypeCheck;
     }
 
-    // Update is called once per frame
-    void Update()
+    void MenuTypeCheck(MenuType menuType)
     {
-        
+        if (true) { }
     }
 
     public void PlayButtonClicked()
     {
         Debug.Log("Clicked play!");
     }
+
+    public void OptionsButtonClicked()
+    {
+        DisableOthers(menuGroup);
+    }
+
+    
 }
