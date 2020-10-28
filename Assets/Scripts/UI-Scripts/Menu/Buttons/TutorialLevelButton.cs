1dd56f2ae57635a850f00171d38ee16d0e31299b
Mrp1 Gamer <mrp1gamerplayz@gmail.com>
2020-10-28 19:57:20 +0530

    Tutorial level

---
+++ Assets/Scripts/UI-Scripts/Menu/Buttons/TutorialLevelButton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TutorialLevelButton : MonoBehaviour
+{
+    [SerializeField] private int levelToOpen;
+    [SerializeField] private GameObject transition;// the black image that slowly goes up in darkness.
+    private bool hasTransitionStarted = false;
+
+    public void OnClick()
+    {
+        transition.GetComponent<Animator>().SetTrigger("CallTransition");
+        hasTransitionStarted = true;
+    }
+
+    private void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    private void Update()
+    {
+        if (hasTransitionStarted)
+        {
+            if (transition.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+            {
+                SceneManager.LoadScene(levelToOpen);
+            }
+        }
+    }
+}
